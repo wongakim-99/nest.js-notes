61a113316321cd6233ee3978ca5b9e2179943bee
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-03-31 12:26:40 +0900

    [#2] - BoardService 생성
    - DI(Dependency Injection)을 통해 Controller 클래스에서 Service 객체 사용하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,22 @@
 import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+    constructor(private boardService: BoardsService) {}
+}
+
+
+// @Controller('boards')
+// export class BoardsController {
+//     boardsService: BoardsService;
+//
+//     constructor(boardsService: BoardsService) {
+//         this.boardsService = boardsService;
+//     }
+//
+//     // @Get()
+//     // getAllTask(): Board[] {
+//     //     return this.boardsService.getAllBoards();
+//     // }
+// }
