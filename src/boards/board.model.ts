52453c6a15b1b5b5875ba141b15f37e09e004698
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-04-01 11:11:05 +0900

    [#3] - Board model 정의
    - 게시물에 필요한 데이터가 무엇이 있는지 정의

---
+++ src/boards/board.model.ts
@@ -0,0 +1,12 @@
+// 변수의 타입만을 정하기 위해서 interface 사용
+export interface Board {
+    id : string;
+    title : string;
+    description : string;
+    status : BoardStatus;
+}
+
+export enum BoardStatus {
+    PUBLIC = 'PUBLIC',
+    PRIVATE = 'PRIVATE',
+}
