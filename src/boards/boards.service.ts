28d700ede378be4d5bab92b9b645ce14b000f1dc
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-04-01 11:25:31 +0900

    [#3] - Service 레이어에서 게시물 생성 메서드 작성
    - id 값 같은 경우 현재 DB가 없기 때문에 uuid 모듈을 다운받아 임시로 설정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,7 +8,21 @@ export class BoardsService {
     //다른 컴포넌트에서 boards 라는 배열 수정 가능성 차단하기 위해 private 선언
     private boards: Board[] = [];  // 게시판에 대한 데이터 담는 배열
 
+    // 전체 게시글 조회 메서드
     getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    // 게시물 생성 메서드
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC,
+        }
+
+        this.boards.push(board);
+        return board;
     }
 }
