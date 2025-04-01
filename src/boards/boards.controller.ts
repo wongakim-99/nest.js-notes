479fd735e5d4ebe1811e4f517911582d48398420
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-04-01 10:47:19 +0900

    [#3] - 모든 게시물 조회 서비스 생성
    - 차후에 DB와 연동, 우선은 로컬 메모리에 담아서 처리
    - 컨트롤러 값 처리

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardService: BoardsService) {}
+
+    @Get('/')
+    getAllBoards(): Board[] {
+        return this.boardService.getAllBoards();
+    }
 }
 
 
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+
+    //다른 컴포넌트에서 boards 라는 배열 수정 가능성 차단하기 위해 private 선언
+    private boards: Board[] = [];  // 게시판에 대한 데이터 담는 배열
+
+    getAllBoards(): Board[] {
+        return this.boards;
+    }
+}
