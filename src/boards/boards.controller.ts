aed9ac3afd40e6c9bb9b9126d33569cf87045842
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-04-02 19:25:31 +0900

    [#3] - interface형 DTO와 class형 DTO의 차이점
    - 이를 테스트 하기 위해서는 아래와 같은 명령어를 입력해주시면 됩니다.
    
    yarn add class-validator class-transformer
    혹은
    npm install class-validator class-transformer

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -13,6 +13,7 @@ export class BoardsController {
     }
 
     @Post()
+    @UsePipes(new ValidationPipe())
     createBoard(
         @Body() createBoardDto: CreateBoardDto
     ): Board {
--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,4 +1,15 @@
+import {IsString} from 'class-validator';
+
 export class CreateBoardDto {
+    @IsString()
     title: string;
+
+    @IsString()
     description: string;
 }
+
+// 만약 DTO 를 class 에서 interface 로 바꾸면 어떤 일이 벌어질까?
+// export interface CreateBoardDto {
+//     title: string;
+//     description: string;
+// }
