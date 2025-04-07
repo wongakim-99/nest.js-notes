c1850079c0ebe5c282f9494ee0fc8336dc4f8639
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-04-07 15:59:42 +0900

    [#6] - 파일 기본 세팅
    - .env 파일 세팅
    - config 관련 추가 패키지 세팅
    - users 관련 기본 CRUD 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    const configService = app.get(ConfigService);
+
+    const tmpPort = configService.get<number>('PORT');
+
+    if (tmpPort === undefined) {
+        throw new Error('PORT 환경변수가 설정되지 않았습니다.')
+    }
+    const port: number = tmpPort;
+
+    // 환경변수 확인용 로그
+    console.log(`Loaded port from .env : ${port}`);
+
+    // 서버가 실제로 어떤 포트로 실행 중인지 출력
+    console.log(`Application is running on : http://localhost:${port}`);
+
+    await app.listen(port);
 }
+
 bootstrap();
